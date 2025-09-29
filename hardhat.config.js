bb2faaca03b1b886a4634aff5982fe7871e624e5
Coinbol <108339907+Coinbol@users.noreply.github.com>
2025-09-29 19:06:09 +0300

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,1 +1,11 @@
+require("@nomicfoundation/hardhat-toolbox");
 
+module.exports = {
+    solidity: "0.8.24",
+    networks: {
+        sepolia: {
+            url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+            accounts: { mnemonic: process.env.MNEMONIC }
+        }
+    }
+};
