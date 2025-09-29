ea5be8fe6d61ba07ac3c7bdeb3005a03ef2e2ab8
Coinbol <108339907+Coinbol@users.noreply.github.com>
2025-09-29 18:53:36 +0300

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,1 +1,10 @@
+const { ethers } = require("hardhat");
 
+async function main() {
+    const root = "0x123..."; // Merkle root (setupWords.js'den al)
+    const SuperEncryptedWordle = await ethers.getContractFactory("SuperEncryptedWordle");
+    const contract = await SuperEncryptedWordle.deploy(root);
+    await contract.waitForDeployment();
+    console.log("Contract deployed:", await contract.getAddress());
+}
+main();
